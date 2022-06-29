ca79d344320282a2f784dbb5b3dfc3a0b32db9f3
TBiannca <bia.topircean@gmail.com>
2022-06-29 21:30:48 +0300

    Run script if descriptors match

--- app.js
+++ app.js
@@ -1,37 +1,42 @@
 const tf = require('@tensorflow/tfjs-node')
 const fs = require('fs')
-const faceapi = require('face-api.js')
+const faceapi = require('@vladmandic/face-api')
 const loadLabeledDescriptors = require('./load-labeled-descriptors')
 const getToken = require('./get-user')
 
 const run = async () => {
-
-  const spawn = require("child_process").spawn;
-  const pythonProcess = spawn('python',["run.py"])
 
   await faceapi.nets.ssdMobilenetv1.loadFromDisk('./public/models')
   await faceapi.nets.faceLandmark68Net.loadFromDisk('./public/models')
   await faceapi.nets.faceRecognitionNet.loadFromDisk('./public/models')
 
   const token = await getToken()
-  console.log(token)
+  console.log(token) 
 
   const verify = tf.node.decodeImage(fs.readFileSync('./public/verify/elon-musk.jpg'))
 
-  const singleResult = await faceapi
-  .detectSingleFace(verify)
-  .withFaceLandmarks()
-  .withFaceDescriptor()
-
-  //console.log('WWWWWWWWWWW', singleResult.descriptor)
+  if(await faceapi.detectSingleFace(verify) !== undefined)
+  {
+    const singleResult = await faceapi
+    .detectSingleFace(verify)
+    .withFaceLandmarks()
+    .withFaceDescriptor()
+    
+    const labeledFaceDescriptors = await loadLabeledDescriptors(token)
+    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
   
-  const labeledFaceDescriptors = await loadLabeledDescriptors(token)
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
+    if (singleResult) {
+      const bestMatch = faceMatcher.findBestMatch(singleResult.descriptor)
+      console.log(bestMatch.toString())
 
-  if (singleResult) {
-    const bestMatch = faceMatcher.findBestMatch(singleResult.descriptor)
-    console.log(bestMatch.toString())
+      if(bestMatch.distance < 0.6)
+      {
+        const spawn = require("child_process").spawn;
+        const pythonProcess = spawn('python',["run.py"])
+      }
+    }
   }
+
 }
 
 
